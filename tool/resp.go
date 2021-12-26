c1d3f9c8c7dd37e21f759ede136db3d4ec0b906f
2164389784@qq.com <2164389784@qq.com>
2021-12-26 18:41:21 +0800

    努力

--- tool/resp.go
+++ tool/resp.go
@@ -10,16 +10,19 @@ func RespInternalError(c *gin.Context) {
 		"info": "服务器错误",
 	})
 }
+
 func RespErrorWithData(c *gin.Context, data interface{}) {
 	c.JSON(http.StatusOK, gin.H{
 		"info": data,
 	})
 }
+
 func RespSuccessful(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"info": "成功！",
 	})
 }
+
 func RespSuccessfulWithData(c *gin.Context, data interface{}) {
 	c.JSON(http.StatusOK, gin.H{
 		"info": "成功",
